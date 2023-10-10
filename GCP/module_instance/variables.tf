327593c363afe5c2138bcb83bd5c51663714fc9a
VA <56908036+vikasannaboina@users.noreply.github.com>
2023-10-10 19:07:10 +0530

    Create variables.tf

---
+++ GCP/module_instance/variables.tf
@@ -0,0 +1,8 @@
+variable "image" {
+    type = string
+    description = "project/family_type of instance image"
+}
+variable "zone" {
+    type = string
+    description = "zone of the instance"
+}
